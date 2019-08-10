38ecdcf828000fdc6df6d7dce81e34a2f1e8bb28
Zu3zz <zthzththth@163.com>
2019-08-10 23:06:39 +0800

    finish entity

---
+++ src/main/java/com/zth/o2o/entity/Area.java
@@ -0,0 +1,61 @@
+package com.zth.o2o.entity;
+
+import java.util.Date;
+
+/**
+ * Author: 3zZ.
+ * Date: 2019-08-09 23:57
+ */
+public class Area {
+    // ID
+    private Integer areaId;
+    // 名称
+    private String areaName;
+    // 权重
+    private Integer priority;
+    // createTime
+    private Date createTime;
+
+    public Integer getAreaId() {
+        return areaId;
+    }
+
+    public void setAreaId(Integer areaId) {
+        this.areaId = areaId;
+    }
+
+    public String getAreaName() {
+        return areaName;
+    }
+
+    public void setAreaName(String areaName) {
+        this.areaName = areaName;
+    }
+
+    public Integer getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Integer priority) {
+        this.priority = priority;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getLastEditTime() {
+        return lastEditTime;
+    }
+
+    public void setLastEditTime(Date lastEditTime) {
+        this.lastEditTime = lastEditTime;
+    }
+
+    // updateTime
+    private Date lastEditTime;
+}
---
+++ src/main/java/com/zth/o2o/entity/WechatAuth.java
@@ -0,0 +1,46 @@
+package com.zth.o2o.entity;
+
+import java.util.Date;
+
+/**
+ * Author: 3zZ.
+ * Date: 2019-08-10 12:33
+ */
+public class WechatAuth {
+    private Long wechatAuthId;
+    private String openId;
+    private Date createTime;
+    private PersonInfo personInfo;
+
+    public Long getWechatAuthId() {
+        return wechatAuthId;
+    }
+
+    public void setWechatAuthId(Long wechatAuthId) {
+        this.wechatAuthId = wechatAuthId;
+    }
+
+    public String getOpenId() {
+        return openId;
+    }
+
+    public void setOpenId(String openId) {
+        this.openId = openId;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public PersonInfo getPersonInfo() {
+        return personInfo;
+    }
+
+    public void setPersonInfo(PersonInfo personInfo) {
+        this.personInfo = personInfo;
+    }
+}
